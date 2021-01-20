2afd5b9bdbe512c92ee0fa3da96adbfecf8bb23e
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-20 14:19:30 +1100

    Add EditableTextarea component with necessary associated styling

---
+++ src/Components/Editable-Textarea.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+/* Wrap around inputs or textareas to make them editable
+State is not handled by either Editable or the child
+input / textarea. */
+class EditableInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditing: this.props.edit === undefined ? false : this.props.edit,
+      value: props.text,
+    }
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.setEditing = this.setEditing.bind(this)
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+  }
+
+  handleKeyDown(e) {
+    if (e.key === "Escape") {
+      this.setEditing(false);
+      return;
+    }
+  }
+
+  handleChangeInput(e) {
+    const inputValue = e.target.value;
+    console.log(inputValue);
+    this.setState({ value: inputValue })
+  }
+
+  setEditing(bool) {
+    this.setState({ isEditing: bool });
+  }
+
+  render() {
+    const value = this.state.value;
+
+    let displayedContent;
+    
+    if (this.state.isEditing) {
+      displayedContent = (
+        <div
+          onBlur={() => this.setEditing(false)}
+          onKeyDown={ (e) => this.handleKeyDown(e)}  
+        >
+          <textarea type="text" value={value} onChange={this.handleChangeInput}></textarea>
+        </div>
+      );
+    } else {
+      displayedContent = (
+        <div onClick={() => this.setEditing(true)}>
+          <span>{ value }</span>
+        </div>
+      );
+    }
+
+    return (
+      <div className={`editable-textarea ${this.props.className ? ` ${this.props.className}` : ''}`}>
+        {displayedContent}
+      </div>
+    );
+  }
+
+}
+
+export default EditableInput;
